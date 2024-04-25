c459616bb3733f6e0ecac02f87e383704b7e1ab6
KhloudHisham <khokhakammar@gmail.com>
2024-04-25 09:38:25 +0200

    initial

---
+++ src/app/productspage/productspage.component.ts
@@ -0,0 +1,28 @@
+import { ProductService } from './../services/product.service';
+import { Component, OnInit } from '@angular/core';
+import { Iproduct } from '../models/iproduct';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-productspage',
+  standalone: true,
+  imports: [CommonModule,RouterLink],
+  templateUrl: './productspage.component.html',
+  styleUrl: './productspage.component.css'
+})
+export class ProductspageComponent implements OnInit{
+ 
+    products: Iproduct[];
+    constructor(public productservice:ProductService) {
+      this.products = [];
+    }
+    ngOnInit(): void {
+      this.products = this.productservice.getAllProducts();
+    }
+  
+    deleteHandler(productId: number) {
+      this.products = this.productservice.deleteProduct(productId);
+    }
+  }
+
